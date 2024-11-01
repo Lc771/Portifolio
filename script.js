a8573fdd92076564c2c8503c3ff677d0c97bf016
eolc7 <159260342+Lc771@users.noreply.github.com>
2024-10-31 22:46:58 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+window.addEventListener("load", function() {
+    const titulo = document.getElementById("titulo");
+    titulo.classList.add("animate-slideUp");
+});
+
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+  };
